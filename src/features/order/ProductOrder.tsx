a422608e344cb3169dae1b075094f4eaedc02b00
rehanshaikh55 <shaikhrehan1016@gmail.com>
2025-03-12 17:44:39 +0530

    added all prices in bill details

--- src/features/order/ProductOrder.tsx
+++ src/features/order/ProductOrder.tsx
@@ -1,16 +1,53 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import CustomHeader from '@components/ui/CustomHeader'
-import { Colors } from '@utils/Constants'
+import { Colors, Fonts } from '@utils/Constants'
 import OrderList from './OrderList'
+import { CustomText } from '@components/ui/customText'
+import  Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { RFValue } from 'react-native-responsive-fontsize'
+import BillDetails from './BillDetails'
+import { useCartStore } from '@state/cartStore'
+import { useAuthStore } from '@state/authStore'
 
 const ProductOrder = () => {
+ const {getTotalPrice,cart,clearCart} = useCartStore();
+ const {user,setCurrentOrder,currentOrder} = useAuthStore();
+  const totalItemPrice = getTotalPrice()
+
   return (
     <View style={styles.container} >
      <CustomHeader title='Checkout' />
      <ScrollView contentContainerStyle={styles.scrollContainer}>
          <OrderList />
+          <View style={styles.flexRowBetween}>
+            <View style={styles.flexRow}>
+                    <Image
+                    source={require('@assets/icons/coupon.png')}
+                    style={{width:25,
+                      height:25
+                    }}
+                    />
+                    <CustomText variant='h6' fontFamily={Fonts.SemiBold}>
+                           Use Coupons
+                    </CustomText>
+            </View>
+            <Icon name='chevron-right' size={RFValue(16)} color={Colors.text} /> 
+          </View>
+          <BillDetails totalItemPrice={totalItemPrice} />
+          <View style={styles.flexRowBetween}>
+            <View>
+              <CustomText variant='h8' fontFamily={Fonts.SemiBold}>
+                Cancellation Policy
+              </CustomText>
+              <CustomText variant='h9' style={styles.cancelText} fontFamily={Fonts.SemiBold}>
+                Orders cannot be cancelled once packed for delivery, In case of unexpected delay, refund will be provided, if applicable
+              </CustomText>
+            </View>
+
+          </View>
      </ScrollView>
+     
     </View>
   )
 }
@@ -18,6 +55,10 @@ const ProductOrder = () => {
 export default ProductOrder
 
 const styles = StyleSheet.create({
+  cancelText:{
+         marginTop:4,
+         opacity:0.6
+  },
     container:{
         flex:1,
         backgroundColor:'#fff'
@@ -25,5 +66,18 @@ const styles = StyleSheet.create({
         backgroundColor:Colors.backgroundSecondary,
         padding:10,
         paddingBottom:250
+    },
+    flexRowBetween:{
+      backgroundColor:'#fff',
+      alignItems:'center',
+      justifyContent:'space-between',
+      padding:10,
+      flexDirection:'row',
+      borderRadius:15
+    },
+    flexRow:{
+      alignItems:'center',
+      flexDirection:'row',
+      gap:10
     }
 })
